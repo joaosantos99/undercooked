a974868a000d8756334169768e03b376bed3105c
JPMini84 <joaopedrotazercraft@hotmail.com>
2023-05-27 22:53:40 +0100

    Bucket

---
+++ Assets/MoveBucketUp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBucketUp : MonoBehaviour
+{
+
+    [SerializeField] private GameObject ancora;
+    [SerializeField] private Transform balde;
+    [SerializeField] private float velocidade = 10f;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        balde.transform.position = Vector2.MoveTowards(balde.transform.position, ancora.transform.position, Time.deltaTime * velocidade);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,100 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    private Vector2 spawnOne = new Vector2(6f, 0f);
-    private Vector2 spawnTwo = new Vector2(0f, 10f);
-
-    private float horizontal;
-    private bool isFacingRight = true;
-
-    private Camera camera;
-
-    [SerializeField] private float speed = 8f;
-    [SerializeField] private float jumpingPower = 16f;
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-
-    void Start()
-    {
-        camera = Camera.main;
-    }
-
-    public override void OnNetworkSpawn()
-    {
-        base.OnNetworkSpawn();
-        calltoclientconectedServerRPC(NetworkManager.Singleton.LocalClientId);
-    }
-
-
-    [ServerRpc(RequireOwnership = false)]
-    public void calltoclientconectedServerRPC(ulong clientId)
-    {
-
-        ClientRpcParams clientRpcParams = new ClientRpcParams
-        {
-            Send = new ClientRpcSendParams
-            {
-                TargetClientIds = new ulong[] { clientId }
-            }
-        };
-
-
-        int playerCount = NetworkManager.Singleton.ConnectedClients.Count;
-        ClientConnectedClientRPC(playerCount, clientRpcParams);
-    }
-
-    [ClientRpc]
-    private void ClientConnectedClientRPC(int playerCount, ClientRpcParams clientRpcParams = default)
-    {
-        if(playerCount == 1)
-        {
-            transform.position = spawnOne;
-            camera.transform.position = new Vector3(0f, -0.2f, -10f);
-        } else
-        {
-            transform.position = spawnTwo;
-            camera.transform.position = new Vector3(0f, 9.5f, -10f);
-        }
-    }
+    public Vector2 Speed = new Vector2(1, 1);
 
     void Update()
     {
         if (!IsOwner) return;
 
-        horizontal = Input.GetAxisRaw("Horizontal");
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
+        Vector2 movement = new Vector2(Speed.x * inputX, Speed.y * inputY);
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-
-        Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
+        transform.Translate(movement);
     }
 }
